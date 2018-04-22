2192e6be7e8ea7a9f507e41c1ff88384c7923875
swag2 <brendonkootz@gmail.com>
2018-04-22 18:25:48 -0500

    Homework

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,11 +6,46 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+	let i = n, k = i * (i-1);
+	i = i - 2;
+	function GetIt(){
+		if(i != 0){
+			k = k * i;
+			i--;
+			return GetIt();
+		} else {
+			return k;
+		}
+	}
+	return GetIt();
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+	
+// ############## DO NOT ERASE THIS###################
+/* The test case is asking for '8' the 6th (7th) number in the Fibocci Array
+	So far this code return the 5th (6th) variable. being '5'. This is so close
+	to working is not even funny*/
+
+	let Stop = n-1, Place1=0, Place2=1;
+	let GR = [1,2]; // GR is for 'Golden Ratio'
+	function AisBisC(){
+		let Here = GR[Place1] + GR[Place2];
+		if(Place2 != n){
+			GR.push(Here);
+	
+			Place1++;
+			Place2++;
+			
+			return AisBisC();
+		} else {
+		
+			return GR[Stop];
+		}
+	}
+	return AisBisC();
 }
 
 // Do not modify code below this line.
